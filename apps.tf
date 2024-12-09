9deb466c1d2c25a1e801a2cc9604a2505e9f5be9
Philip Lalonde <lalondeph@gmail.com>
2024-12-09 14:06:58 -0500

    add in prod pilot yaml and an apps.tf file

---
+++ apps.tf
@@ -0,0 +1,21 @@
+locals {
+  is_prod = var.google_project_id != "production"
+  apps = local.is_prod ? yamldecode(file("${path.root}/apps/prod.yaml")) : yamldecode(file("${path.root}/apps/pilot.yaml"))
+}
+
+data "google_project" "project" {}
+
+resource "google_storage_bucket" "cloud-fn-source" {
+  name = "cloud-fn-source"
+  project = var.google_project_id
+  location = var.project_region
+  uniform_bucket_level_access = true
+  public_access_prevention = "enforced"
+}
+
+resource "google_project_iam_member" "members-from-yaml" {
+  for_each = local.apps.members
+  project = var.google_project_id
+  role = each.value.role
+  member = each.value.member
+}
