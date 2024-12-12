63bf8fcb3aca9a7a8e2415ff03eeb9b96d156c3c
Philip Lalonde <lalondeph@gmail.com>
2024-12-12 15:11:23 -0500

    edit cloud function

--- golang/cloud-function/main.go
+++ golang/cloud-function/main.go
@@ -16,10 +16,3 @@ func HandleRequest(ctx context.Context) error {
 	return nil
 }
 
-func main() {
-	// Simulate deployment
-	ctx := context.Background()
-	if err := HandleRequest(ctx); err != nil {
-		log.Fatalf("Error executing cloud function: %v", err)
-	}
-}
